bfd9bd1084486503839aaa87d65ea0d1b0a5485f
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-06-07 20:08:05 -0400

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Login from './Paginas/Login';
+import Search from './Paginas/Search';
+import Album from './Paginas/Album';
+import Favorites from './Paginas/Favorites';
+import Profile from './Paginas/Profile';
+import ProfileEdit from './Paginas/ProfileEdit';
+import NotFound from './Paginas/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
-    // teste
+    return (
+      <div>
+        <p>TrybeTunes</p>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" render={ () => <Login /> } />
+            <Route exact path="/search" render={ () => <Search /> } />
+            <Route exact path="/album/:id" render={ () => <Album /> } />
+            <Route exact path="/favorites" render={ () => <Favorites /> } />
+            <Route exact path="/profile" render={ () => <Profile /> } />
+            <Route exact path="/profile/edit" render={ () => <ProfileEdit /> } />
+            <Route exact path="*" render={ () => <NotFound /> } />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
---
+++ src/Paginas/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NofFound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        page-not-found
+      </div>
+    );
+  }
+}
+
+export default NofFound;
