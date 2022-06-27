b18331d2340fbef8e369f13af3a88773167a0e9c
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-06-27 15:14:42 -0400

    3

--- src/Paginas/Login.js
+++ src/Paginas/Login.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
-class Login extends React.Component {
+import { createUser } from '../services/userAPI';
+import LoadingMsg from './LoadingMsg';
+
+class Login extends Component {
+  state = {
+    name: '',
+    buttonDisabled: true,
+    loadingMsg: false,
+    redirected: false,
+  }
+
+  handleChange = (event) => {
+    const valor = event.target.value;
+    this.setState({ name: valor });
+    if (valor.length > 2) {
+      this.setState({ buttonDisabled: false });
+    } else {
+      this.setState({ buttonDisabled: true });
+    }
+  }
+
+  handleClick = async () => {
+    const { name } = this.state;
+    this.setState({ loadingMsg: true });
+    await createUser({ name });
+    this.setState({ redirected: true });
+  }
+
   render() {
+    const { redirected, loadingMsg, buttonDisabled } = this.state;
     return (
       <div data-testid="page-login">
-        Login
+        <form>
+          <textarea
+            placeholder="Insira seu nome aqui"
+            data-testid="login-name-input"
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="login-submit-button"
+            type="button"
+            disabled={ buttonDisabled }
+            onClick={ this.handleClick }
+          >
+            Entrar
+          </button>
+        </form>
+        { redirected && <Redirect to="/search" /> }
+        { loadingMsg && <LoadingMsg /> }
       </div>
     );
   }
